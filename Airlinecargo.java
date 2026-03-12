e91cebeea8e48f6c1b8144658c81ba8da6846a19
bhuvanapuyalnathan2007-netizen <bhuvanapuyalnathan2007@gmail.com>
2026-03-12 23:50:37 +0530

    Add files via upload

---
+++ Airlinecargo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Airlinecargo {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int weight=sc.nextInt();
+        int rate=sc.nextInt();
+        int priority=sc.nextInt();
+        int insurance=sc.nextInt();
+        int handling=sc.nextInt();
+        int fuel=sc.nextInt();
+
+        int Revenue=(weight*rate)+priority+insurance-handling-fuel;
+        System.out.println("Cargo Revenue="+Revenue);
+        sc.close();
+    }
+}
