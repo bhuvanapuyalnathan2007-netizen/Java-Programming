debb6ffdef94f8b78c6fba25eae0c3e1f50aa63c
bhuvanapuyalnathan2007-netizen <bhuvanapuyalnathan2007@gmail.com>
2026-07-22 15:09:38 +0530

    Add files via upload

---
+++ Silicon.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Silicon
+{
+	public static void A(){
+	    System.out.println("hi hello");
+	    System.out.println("how are u");
+	}
+	public static void main(String[] args){
+	    Main.A();
+	    System.out.println("..........");
+	    A();
+	}
+	
+}
+
